03dbe4e6d120d705f6fb9fbecd3bb402520d8089
You-NaSong <twins7534@naver.com>
2024-08-27 15:03:21 +0900

    Add : 스크롤 이벤트 넣기

--- vite-react-app/src/App.tsx
+++ vite-react-app/src/App.tsx
@@ -1,22 +1,26 @@
 
+import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
 import About from './components/About';
 import Features from './components/Features';
 import Portfolio from './components/Portfolio';
 import Contact from './components/Contact';
+import useScrollAnimation from './hook/UseScrollAnimation';
 
-const App = (): JSX.Element => {
-  return (
-    <div>
-      <Header />
-      <Main />
-      <About />
-      <Features />
-      <Portfolio />
-      <Contact />
-    </div>
-  );
+const App: React.FC = () => {
+    useScrollAnimation();
+
+    return (
+        <>
+          <Header />
+          <Main />
+          <About />
+          <Features />
+          <Portfolio />
+          <Contact />
+        </>
+    );
 };
 
 export default App;
